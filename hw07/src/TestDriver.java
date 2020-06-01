6c6c1bae12adeb4527f0d7b7d0b72eebdf925f93
edward <edward.yu0830@gmail.com>
2020-06-01 10:47:29 +0800

    重新處理adjList部分

--- hw07/src/TestDriver.java
+++ hw07/src/TestDriver.java
@@ -1,18 +1,21 @@
-
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 public class TestDriver {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
+		ArrayList<ArrayList<Integer>> arr = new ArrayList<ArrayList<Integer>>();
 		LSPath entity1 = new LSPath4107056006_1();
-		int[][] array = {{0,1},
-						{1,2},
-						{1,3},
-						{2,1},
-						{2,4},
-						{3,1},
-						{3,4},
-						{4,2},
-						{4,3}};
+		BufferedReader bfr = new BufferedReader(new FileReader("C:\\Users\\User\\git\\Algorithm\\hw07\\data\\testdata1.txt"));
+		while(bfr.ready()) {
+			String t = bfr.readLine();
+			Scanner scn = new Scanner(t);
+			ArrayList<Integer> temp = new ArrayList<Integer>();
+			temp.add(scn.nextInt());temp.add(scn.nextInt());
+			arr.add(temp);
+		}
+		int[][] array = arr.stream().map(  u  ->  u.stream().mapToInt(i->i).toArray()  ).toArray(int[][]::new);
 		int ans = entity1.Ans(array);
 		System.out.println("ans is:"+ans);
 	}
